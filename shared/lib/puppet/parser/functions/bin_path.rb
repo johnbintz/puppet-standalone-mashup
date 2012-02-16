4c14b0865b1c543809aeabf59ec60222900fb98b
John Bintz <john@coswellproductions.com>
2012-02-15 20:33:18 -0500

    a thing

--- shared/lib/puppet/parser/functions/bin_path.rb
+++ shared/lib/puppet/parser/functions/bin_path.rb
@@ -1,7 +1,11 @@
 require 'puppet/modules/common_directories'
 
 module Puppet::Parser::Functions
+  $stdout.puts "here"
+
   newfunction(:bin_path, :type => :rvalue) do |args|
+    $stdout.puts args.inspect
+
     bin_path(lookupvar('base::install_path'), *args)
   end
 end
