171b4d3546b2c6bdcbd1495709a6d124d9efee0f
John Bintz <john@coswellproductions.com>
2012-05-14 11:07:28 -0400

    clean up varnish config

--- shared/additional-modules/varnish/manifests/debian.pp
+++ shared/additional-modules/varnish/manifests/debian.pp
@@ -1,21 +1,34 @@
-class varnish::debian($version, $vcl_template) {
-  $user = 'varnish'
-  $group = 'varnish'
+class varnish::debian($version, $vcl_template, $store_file_mb = 1024) {
+  $varnish_user = 'varnish'
+  $varnish_group = 'varnish'
 
-  user { $user: uid => 27835 }
+  user { $varnish_user: uid => 27835 }
 
-  package { 'libpcre3-dev': ensure => installed }
+  $packages = [ 'libpcre3', 'libpcre3-dev', 'pkg-config' ]
+
+  package { $packages: ensure => installed }
 
   class { 'varnish':
     version => $version,
-    require => Package['libpcre3-dev'],
+    require => Package[$packages],
     vcl_template => $vcl_template
   }
 
-  init_d { 'varnish':
-    init_d_prerun => template('varnish/debian/init-d-prerun'),
+  init_d_bundle { 'varnish':
     init_d_prolog => template('varnish/debian/init-d-prolog'),
+    init_d_prerun => template('varnish/debian/init-d-prerun'),
     require => Class['varnish']
+  }
+
+  exec { 'ensure-data-store-ownership':
+    command => "chown -R ${varnish_user}:${varnish_group} ${varnish::data}",
+    path => $base::path,
+    require => Class['varnish']
+  }
+
+  logrotate_d { 'varnishncsa':
+    postrotate => 'service varnish rotate',
+    pattern => "${varnish::log}/access.log"
   }
 }
 
