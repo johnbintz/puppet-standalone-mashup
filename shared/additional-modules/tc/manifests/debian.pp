32af825f62385d3050cd78c699a4f07175bff492
John Bintz <john@coswellproductions.com>
2012-05-17 16:40:21 -0400

    forgot to install ethtool

--- shared/additional-modules/tc/manifests/debian.pp
+++ shared/additional-modules/tc/manifests/debian.pp
@@ -1,6 +1,8 @@
 class tc::debian($config) {
   $packages = [ 'ethtool' ]
 
+  package { $packages: ensure => installed }
+
   init_d_bundle { 'tc':
     init_d_prolog => template('tc/debian/init-d-prolog'),
     init_d_prerun => template('tc/debian/init-d-prerun'),
